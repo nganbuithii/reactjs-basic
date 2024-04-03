ac36d8f92a82a86578c63e3c10fcb5d2ddd5f1fb
Ngân <nganbui.23112003@gmail.com>
2024-04-03 23:00:25 +0700

    component cơ bản

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
 
+
+// Tách thành component -> để viết cái chung
+function Feature(){
+  return (
+    <div className='feature'>
+      <img src='' alt='' className='feature-img' />
+      <h3 className="feature-title"> Heloo bạn </h3>
+      <p className='feature-desc'> kurfo23uori34yyt34ot43</p>
+    </div>
+  )
+}
 function App() {
   //const name =" Ngan xink"
-  return <div className='feature'>
-    <img src='' alt='' className='feature-img' />
-    <h3 className="feature-title"> Heloo bạn </h3>
-    <p className='feature-desc'> kurfo23uori34yyt34ot43</p>
+  return (
+    <div>
+
+    {/* Cac feature này được gọi là component */}
+    <Feature></Feature>
+    <Feature></Feature>
   </div>
+  )
+  
 }
 
 export default App;
