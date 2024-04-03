e639c9ef3317632bb2139215f1daa6130a311aa4
Ngân <nganbui.23112003@gmail.com>
2024-04-04 01:12:44 +0700

    update STATE

--- src/App.js
+++ src/App.js
@@ -3,20 +3,14 @@ import './App.css';
 import { YoutubeData } from './YoutubeData';
 import YoutubeItem from './components/Youtube/YoutubeItem';
 import YoutubeList from './components/Youtube/YoutubeList';
+import Toggle from './components/State/Toggle';
 function App() {
-  // Example: RENDER LIST
-  // const numbers = [1,2,3,4,5,6];
-  // const double = numbers.map((item) => item * 2);
-  // console.log(double);
-  //console.log(YoutubeData);
-  const name= " Ngan"
+
   return (
-    <YoutubeList>
-      {/* Children 
-      prop. children : truyền ở giữa component*/}
-      <h1>rùhefijfwi43</h1>
-      { name}
-    </YoutubeList>
+    <>
+      <Toggle></Toggle>
+      <Toggle></Toggle>
+    </>
   )
   
 }
--- src/components/State/Toggle.js
+++ src/components/State/Toggle.js
@@ -1,17 +1,46 @@
-//  Tìm hiểu về STATE
+// //  Tìm hiểu về STATE
 
-// STATELESS FUNCTIONAL COMPONENT: component nhưng KHÔNg sd STATE
-import React, { useState } from "react";
+import { useState } from "react"
+
+// // STATELESS FUNCTIONAL COMPONENT: component nhưng KHÔNg sd STATE
+// import React, { useState } from "react";
+
+// function Toggle(){
+//     return <div className="toggle"></div>
+// }
+
+
+// //STATEFULL FUNCTIONAL COMPONENT: sử dụng STATE
+// function toggle2(){
+//     const [Count, setCount ] = useState();
+//     return (
+//         <div className="toggle"></div>
+//     )
+// }
+
 
 function Toggle(){
-    return <div className="toggle"></div>
-}
+    // 1. enabel state : useState(...)
+    // 2. init state: useState(true hay false), number(1,2,3,4), undefine, null, [1,2,3], {object}
+    // 3.read state: 
+    // 4. update
+    // const a = useState(false);
+    // console.log(a);
 
+    const [on, setOn] = useState(false);
+    // nên đặt tên như on và setOn
+    // off, setOff
+    //active, setActive
+    console.log(on);
+    
 
-//STATEFULL FUNCTIONAL COMPONENT: sử dụng STATE
-function toggle2(){
-    const [Count, setCount ] = useState();
+    //SetOn ở đây là CALLBACK
+
+    //RE RENDER:
     return (
-        <div className="toggle"></div>
+        <div className="toggle" onClick={() => setOn(true)}>Toggle{on ? "ON":"OFF"}</div>
     )
 }
+
+
+export default Toggle;
