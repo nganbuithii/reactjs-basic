f8beafe72e102107f3727f9505f9d57acea789d2
Ngân <nganbui.23112003@gmail.com>
2024-04-04 00:39:34 +0700

    rendering list , Đổ data mảng dữ liệu ra

--- src/App.js
+++ src/App.js
@@ -1,32 +1,31 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { YoutubeData } from './YoutubeData';
 
-
-// Tách thành component -> để viết cái chung
-// Feature là con component
-function Feature(){
-  return (
-    <div className='feature'>
-      <img src='' alt='' className='feature-img' />
-      <h3 className="feature-title"> Heloo bạn </h3>
-      <p className='feature-desc'> kurfo23uori34yyt34ot43</p>
-    </div>
-  )
-}
-// prop là cái gi?
-// là thuộc tính
-// App là cha component
 function App() {
-  //const name =" Ngan xink"
+  // Example: RENDER LIST
+  // const numbers = [1,2,3,4,5,6];
+  // const double = numbers.map((item) => item * 2);
+  // console.log(double);
+  //console.log(YoutubeData);
+
   return (
     <div className='ytb-list'>
 
+    {/* Đổ dữ liệu ra */}
+    {YoutubeData.map((item, index) => (
+      // Duyệt ra các phần tử
+      //<div key={item.id}>Hello {index+1} </div>
+      <YoutubeItem key={item.id} src={item.src} title={item.title} desc={item.desc}></YoutubeItem>
+
+    ))}
+
+    {/* <YoutubeItem src="https://cdn.dribbble.com/users/664697/screenshots/3207635/media/d8b25359a7ea951790187a8d2bf953fd.png?resize=800x600&vertical=center" title="Học lập trình" desc="mô tả cách học lập trình dễ hiểu" author="BTNgan"></YoutubeItem>
     <YoutubeItem src="https://cdn.dribbble.com/users/664697/screenshots/3207635/media/d8b25359a7ea951790187a8d2bf953fd.png?resize=800x600&vertical=center" title="Học lập trình" desc="mô tả cách học lập trình dễ hiểu" author="BTNgan"></YoutubeItem>
     <YoutubeItem src="https://cdn.dribbble.com/users/664697/screenshots/3207635/media/d8b25359a7ea951790187a8d2bf953fd.png?resize=800x600&vertical=center" title="Học lập trình" desc="mô tả cách học lập trình dễ hiểu" author="BTNgan"></YoutubeItem>
     <YoutubeItem src="https://cdn.dribbble.com/users/664697/screenshots/3207635/media/d8b25359a7ea951790187a8d2bf953fd.png?resize=800x600&vertical=center" title="Học lập trình" desc="mô tả cách học lập trình dễ hiểu" author="BTNgan"></YoutubeItem>
     <YoutubeItem src="https://cdn.dribbble.com/users/664697/screenshots/3207635/media/d8b25359a7ea951790187a8d2bf953fd.png?resize=800x600&vertical=center" title="Học lập trình" desc="mô tả cách học lập trình dễ hiểu" author="BTNgan"></YoutubeItem>
-    <YoutubeItem src="https://cdn.dribbble.com/users/664697/screenshots/3207635/media/d8b25359a7ea951790187a8d2bf953fd.png?resize=800x600&vertical=center" title="Học lập trình" desc="mô tả cách học lập trình dễ hiểu" author="BTNgan"></YoutubeItem>
-    <YoutubeItem src="https://cdn.dribbble.com/users/664697/screenshots/3207635/media/d8b25359a7ea951790187a8d2bf953fd.png?resize=800x600&vertical=center" title="Học lập trình" desc="mô tả cách học lập trình dễ hiểu" author="BTNgan"></YoutubeItem>
+    <YoutubeItem src="https://cdn.dribbble.com/users/664697/screenshots/3207635/media/d8b25359a7ea951790187a8d2bf953fd.png?resize=800x600&vertical=center" title="Học lập trình" desc="mô tả cách học lập trình dễ hiểu" author="BTNgan"></YoutubeItem> */}
   </div>
   )
   
---
+++ src/YoutubeData.js
@@ -0,0 +1,24 @@
+
+
+
+export const YoutubeData = [
+    {
+        id:1,
+        title:" ví dụ 1",
+        desc: " mô tả bài toán ehfi3i34jgio43",
+        src:"https://cdn.dribbble.com/users/664697/screenshots/3207635/media/d8b25359a7ea951790187a8d2bf953fd.png?resize=800x600&vertical=center"
+    },
+    {
+        id:2,
+        title:" ví dụ 2",
+        desc: " mô tả bài toán ehfgg5g5i3i34jgio43",
+        src:"https://cdn.dribbble.com/users/664697/screenshots/3207635/media/d8b25359a7ea951790187a8d2bf953fd.png?resize=800x600&vertical=center"
+    },
+    {
+        id:3,
+        title:" ví dụ 3",
+        desc: " mô tả bài toán ehfi3i34jgio43",
+        src:"https://cdn.dribbble.com/users/664697/screenshots/3207635/media/d8b25359a7ea951790187a8d2bf953fd.png?resize=800x600&vertical=center"
+    },
+
+]
