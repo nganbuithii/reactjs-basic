2e1ac343e27876a783fd0a8f6405fdc4377214ea
Ngân <nganbui.23112003@gmail.com>
2024-04-03 23:13:01 +0700

    Thực hành props- thuộc tính

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 
 
 // Tách thành component -> để viết cái chung
+// Feature là con component
 function Feature(){
   return (
     <div className='feature'>
@@ -12,17 +13,29 @@ function Feature(){
     </div>
   )
 }
+// prop là cái gi?
+// là thuộc tính
+// App là cha component
 function App() {
   //const name =" Ngan xink"
   return (
     <div>
 
-    {/* Cac feature này được gọi là component */}
-    <Feature></Feature>
-    <Feature></Feature>
+    <YoutubeItem src="https://cdn.dribbble.com/users/664697/screenshots/3207635/media/d8b25359a7ea951790187a8d2bf953fd.png?resize=800x600&vertical=center" title="Học lập trình" desc="mô tả cách học lập trình dễ hiểu" author="BTNgan"></YoutubeItem>
   </div>
   )
   
 }
 
+function YoutubeItem(props){
+  return (
+    <div className='ytb-item'>
+      <img src={props.src} />
+      <div className='ytb-title'>  {props.title} </div>
+      <div className='ytb-author'>{props.author}</div>
+      <div className='ytb-desc'>{props.desc}</div>
+      
+    </div>
+  )
+}
 export default App;
