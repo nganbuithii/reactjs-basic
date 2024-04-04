384c5115c00f177fc289175b6766016dc9d24f52
Ngân <nganbui.23112003@gmail.com>
2024-04-04 13:31:01 +0700

    Chương 4 - module css

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 
 import './App.css';
-import Counter from './components/Counter/counter';
-import Toggle from './components/State/Toggle';
-import Game from './components/TicTacToe/game';
+import Button from './components/Button/Button';
 function App() {
 
   return (
     <>
-      <Game></Game>
+      <Button >primary</Button>
     </>
   )
   
