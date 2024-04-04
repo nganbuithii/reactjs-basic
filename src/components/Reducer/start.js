5084845b73253fc9ca36951f4077ca9691ba3a06
Ngân <nganbui.23112003@gmail.com>
2024-04-04 11:25:14 +0700

    useReducer

---
+++ src/components/Reducer/start.js
@@ -0,0 +1,12 @@
+// const { useReducer } = require("react");
+
+// // use Reducer 
+// const [state, dispatch] = useReducer(reducer, initState)
+// const action = {type:'CLICK' , payload: {}}
+
+
+// - initSTATE là 1 obj
+// const init = {
+//  board = Array(9).fill(null)
+// xIsNext:true;
+//}
